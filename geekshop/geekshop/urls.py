1af73287c9d4ffab8336501e9463e98821429642
european-54 <artik-54@mail.ru>
2021-09-05 08:15:21 +0700

    Add files via upload

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,16 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', mainapp.products, name='product'),
     path('contact/', mainapp.contact, name='contact'),
-    #path('test/', mainapp.menu, name='menu'),
+    path('test/', mainapp.menu, name='menu'),
     path('', mainapp.main, name='main'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
