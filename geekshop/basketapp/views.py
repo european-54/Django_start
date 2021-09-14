59dbea7f3495a085121f5c47aca4c557c2696cc5
european <artik-54@mail.ru>
2021-09-15 00:13:35 +0700

    #15.09.2021
    #Lazarev Igor
    #Домашнаяя №6

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -3,6 +3,9 @@ from basketapp.models import Basket
 from mainapp.models import Product
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
 
 
 @login_required
@@ -49,3 +52,32 @@ def basket_remove(request, pk):
     basket_record.delete()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def int(quantity):
+    pass
+
+
+@login_required
+def basket_edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        new_basket_item = Basket.objects.get(pk=int(pk))
+
+        if quantity > 0:
+            new_basket_item.quantity = quantity
+            new_basket_item.save()
+        else:
+            new_basket_item.delete()
+
+        basket_items = Basket.objects.filter(user=request.user). \
+            order_by('product__category')
+
+        content = {
+            'basket_items': basket_items,
+        }
+
+        result = render_to_string('basketapp/includes/inc_basket_list.html',\
+                                  content)
+
+        return JsonResponse({'result': result})
