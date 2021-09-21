8a61796e4191c9850bdfc230c222ef460d51176f
european <artik-54@mail.ru>
2021-09-21 16:52:31 +0700

    #21.09.2021
    #Lazarev Igor
    #Домашнаяя №7

---
+++ geekshop/adminapp/views.py
@@ -0,0 +1,45 @@
+from authapp.models import ShopUser
+from django.shortcuts import get_object_or_404, render
+from mainapp.models import Product, ProductCategory
+
+
+
+def users(request):
+    title = 'админка/пользователи'
+
+    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+
+    content = {
+        'title': title,
+        'objects': users_list
+    }
+
+    return render(request, 'adminapp/users.html', content)
+
+
+def user_create(request):
+    pass
+
+
+def user_update(request, pk):
+    pass
+
+
+def user_delete(request, pk):
+    pass
+
+
+def categories(request):
+    title = 'админка/категории'
+
+    categories_list = ProductCategory.objects.all()
+
+    content = {
+        'title': title,
+        'objects': categories_list
+    }
+
+    return render(request, 'adminapp/categories.html', content)
+
+
+def category_create(request):
