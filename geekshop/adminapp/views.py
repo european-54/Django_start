931a92360b44d0430aed948ac55737b6256ebf2b
european <artik-54@mail.ru>
2021-09-21 16:56:08 +0700

    #21.09.2021
    #Lazarev Igor
    #Домашнаяя №7

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -43,3 +43,36 @@ def categories(request):
 
 
 def category_create(request):
+    pass
+
+def category_update(request, pk):
+    pass
+
+def category_delete(request, pk):
+    pass
+
+def products(request, pk):
+    title = 'админка/продукт'
+
+    category = get_object_or_404(ProductCategory, pk=pk)
+    products_list = Product.objects.filter(category__pk=pk).order_by('name')
+
+    content = {
+        'title': title,
+        'category': category,
+        'objects': products_list,
+    }
+
+    return render(request, 'adminapp/products.html', content)
+
+def product_create(request, pk):
+    pass
+
+def product_read(request, pk):
+    pass
+
+def product_update(request, pk):
+    pass
+
+def product_delete(request, pk):
+    pass
