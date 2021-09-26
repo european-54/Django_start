ff30ebc705ca7990224fe0f552d16bf6e0b729c9
european <artik-54@mail.ru>
2021-09-26 18:03:31 +0700

    #26.09.2021
    #Lazarev Igor
    #Домашнаяя №8

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -7,8 +7,17 @@ from django.urls import reverse
 from authapp.forms import ShopUserRegisterForm
 from adminapp.forms import ShopUserAdminEditForm
 from adminapp.forms import ProductEditForm
+from django.views.generic.list import ListView
+from django.utils.decorators import method_decorator
 
 
+class UsersListView(ListView):
+    model = ShopUser
+    template_name = 'adminapp/users.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 @user_passes_test(lambda u: u.is_superuser)
