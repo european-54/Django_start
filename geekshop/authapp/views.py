caafc8877d50989f77939a798d93904292e12992
european <artik-54@mail.ru>
2021-09-29 02:47:29 +0700

    #29.09.2021
    #Lazarev Igor
    #Домашнаяя №1

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -87,3 +87,18 @@ def send_verify_mail(user):
 {settings.DOMAIN_NAME} перейдите по ссылке: \n{settings.DOMAIN_NAME}{verify_link}'
 
     return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+def verify(request, email, activation_key):
+    try:
+        user = ShopUser.objects.get(email=email)
+        if user.activation_key == activation_key and not user.is_activation_key_expired():
+            user.is_active = True
+            user.save()
+            auth.login(request, user)
+            return render(request, 'authapp/verification.html')
+        else:
+            print(f'error activation user: {user}')
+            return render(request, 'authapp/verification.html')
+    except Exception as e:
+        print(f'error activation user : {e.args}')
+        return HttpResponseRedirect(reverse('main'))
