d88e6760cace3d385ad467abc241a0fa6840936f
european <artik-54@mail.ru>
2021-10-04 02:18:13 +0700

    #3.10.2021
    #Lazarev Igor
    #Домашнаяя №2

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -1,14 +1,15 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.timezone import now
 from datetime import timedelta
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users.avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name='Возраст')
-
-    activation_key = models.CharField(max_length=128, blank=True)
-    activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
+    age = models.PositiveIntegerField(verbose_name='Возраст', default=18)
+    activation_key = models.CharField(verbose_name = 'ключ подтверждения', max_length=128, blank=True)
+    activation_key_expires = models.DateTimeField(
+        verbose_name = 'актуальность ключа',
+        default=(now() + timedelta(hours=48)))
 
     def is_activation_key_expired(self):
         if now() <= self.activation_key_expires:
