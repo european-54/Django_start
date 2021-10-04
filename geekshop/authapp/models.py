00d7a14e404e25a21b047a559a27eb3d3458e5b4
european <artik-54@mail.ru>
2021-10-05 00:39:37 +0700

    #4.10.2021
    #Lazarev Igor
    #Домашнаяя №2

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -2,6 +2,8 @@
 #from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 from datetime import timedelta
 
@@ -32,4 +34,14 @@ class ShopUserProfile(models.Model):
     user = models.OneToOneField(ShopUser, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
     tagline = models.CharField(verbose_name='теги', max_length=128, blank=True)
     about_me = models.TextField(verbose_name='о себе', max_length=512, blank=True)
-    about_me = models.CharField(verbose_name='пол', max_length=1, blank=True, choices=GENDER_CHOICES)
+    gender = models.CharField(verbose_name='пол', max_length=1, blank=True, choices=GENDER_CHOICES)
+
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
