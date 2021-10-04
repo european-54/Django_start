daf5e93b1c210c7ced8b694f6d319ebe05d1b3d1
european <artik-54@mail.ru>
2021-10-05 02:22:38 +0700

    #5.10.2021
    #Lazarev Igor
    #Домашнаяя №2

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -7,6 +7,9 @@ from authapp.models import ShopUser
 from authapp.forms import ShopUserLoginForm
 from authapp.forms import ShopUserRegisterForm
 from authapp.forms import ShopUserEditForm
+from django.db import transaction
+from authapp.forms import ShopUserProfileEditForm
+
 
 
 def login(request):
@@ -102,3 +105,30 @@ def verify(request, email, activation_key):
     except Exception as e:
         print(f'error activation user : {e.args}')
         return HttpResponseRedirect(reverse('main'))
+
+
+@transaction.atomic
+def edit(request):
+    title = 'редактирование'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, \
+                                     instance=request.user)
+        profile_form = ShopUserProfileEditForm(request.POST, \
+                                     instance=request.user.shopuserprofile)
+        if edit_form.is_valid() and profile_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+        profile_form = ShopUserProfileEditForm(
+            instance=request.user.shopuserprofile
+        )
+
+    content = {
+        'title': title,
+        'edit_form': edit_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'authapp/edit.html', content)
