47e0ff6c52faa1abd3370e08b247e7cae293d03a
european <artik-54@mail.ru>
2021-10-06 21:01:29 +0700

    #6.10.2021
    #Lazarev Igor
    #Домашнаяя №3

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -12,10 +12,54 @@ from ordersapp.models import Order, OrderItem
 from ordersapp.forms import OrderItemForm
 
 
+class OrderItemsCreate(CreateView):
+   model = Order
+   fields = []
+   success_url = reverse_lazy('ordersapp:orders_list')
+
+   def get_context_data(self, **kwargs):
+       data = super(OrderItemsCreate, self).get_context_data(**kwargs)
+       OrderFormSet = inlineformset_factory(Order, OrderItem, \
+                                            form=OrderItemForm, extra=1)
+
+       if self.request.POST:
+           formset = OrderFormSet(self.request.POST)
+       else:
+           basket_items = Basket.get_items(self.request.user)
+           if len(basket_items):
+               OrderFormSet = inlineformset_factory(Order, OrderItem, \
+                                  form=OrderItemForm, extra=len(basket_items))
+               formset = OrderFormSet()
+               for num, form in enumerate(formset.forms):
+                   form.initial['product'] = basket_items[num].product
+                   form.initial['quantity'] = basket_items[num].quantity
+               basket_items.delete()
+           else:
+               formset = OrderFormSet()
+
+       data['orderitems'] = formset
+       return data
+
+   def form_valid(self, form):
+       context = self.get_context_data()
+       orderitems = context['orderitems']
+
+       with transaction.atomic():
+           form.instance.user = self.request.user
+           self.object = form.save()
+           if orderitems.is_valid():
+               orderitems.instance = self.object
+               orderitems.save()
+
+       # удаляем пустой заказ
+       if self.object.get_total_cost() == 0:
+           self.object.delete()
+
+       return super(OrderItemsCreate, self).form_valid(form)
+
+
 class OrderList(ListView):
    model = Order
 
    def get_queryset(self):
        return Order.objects.filter(user=self.request.user)
-
-
