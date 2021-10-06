a39dea3c5740d7f9c6f614e12e837e94c65f7622
european <artik-54@mail.ru>
2021-10-06 21:29:04 +0700

    #6.10.2021
    #Lazarev Igor
    #Домашнаяя №3

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -66,6 +66,13 @@ class OrderRead(DetailView):
        context['title'] = 'заказ/просмотр'
        return context
 
+   def order_forming_complete(request, pk):
+       order = get_object_or_404(Order, pk=pk)
+       order.status = Order.SENT_TO_PROCEED
+       order.save()
+
+       return HttpResponseRedirect(reverse('ordersapp:orders_list'))
+
 
 class OrderDelete(DeleteView):
    model = Order
