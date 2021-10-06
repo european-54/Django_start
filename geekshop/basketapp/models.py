3f0956833f2b55078822d282164c1deb71c74c74
european <artik-54@mail.ru>
2021-10-06 22:53:47 +0700

    #6.10.2021
    #Lazarev Igor
    #Домашнаяя №3

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -16,50 +16,58 @@ objects = BasketQuerySet.as_manager()
 
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-on_delete=models.CASCADE, related_name='basket')
+    on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество',
-default=0)
+    default=0)
     add_datetime = models.DateTimeField(verbose_name='время',
-auto_now_add=True)
+    auto_now_add=True)
+
+    def property(args):
+        pass
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.product.quantity -= self.quantity - \
+                                     self.__class__.get_item(self.pk).quantity
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
 
 
-def property(args):
-    pass
+    @property
+    def product_cost(self):
+        "return cost of all products this type"
+        return self.product.price * self.quantity
 
 
-@property
-def product_cost(self):
-    "return cost of all products this type"
-    return self.product.price * self.quantity
+    def sum(param):
+        pass
 
 
-def sum(param):
-    pass
+    def list(param):
+        pass
 
 
-def list(param):
-    pass
+    def map(param, _items):
+        pass
 
 
-def map(param, _items):
-    pass
+    def property(args):
+        pass
 
 
-def property(args):
-    pass
+    @property
+    def total_quantity(self):
+        "return total quantity for user"
+        _items = Basket.objects.filter(user=self.user)
+        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+        return _totalquantity
 
-
-@property
-def total_quantity(self):
-    "return total quantity for user"
-    _items = Basket.objects.filter(user=self.user)
-    _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-    return _totalquantity
-
-@property
-def total_cost(self, total=None):
-    "return total cost for user"
-    _items = Basket.objects.filter(user=self.user)
-    _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-    return _totalcost
+    @property
+    def total_cost(self, total=None):
+        "return total cost for user"
+        _items = Basket.objects.filter(user=self.user)
+        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+        return _totalcost
