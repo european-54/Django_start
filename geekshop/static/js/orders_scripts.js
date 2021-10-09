ca13524098b61175371cbb999fa7c52b8f42df39
european <artik-54@mail.ru>
2021-10-09 22:29:44 +0700

    #10.10.2021
    #Lazarev Igor
    #Домашнаяя №5

--- geekshop/static/js/orders_scripts.js
+++ geekshop/static/js/orders_scripts.js
@@ -56,7 +56,42 @@ $('.order_form').on('click', 'input[type="checkbox"]', function () {
    orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
 });
 
-function orderSummaryUpdate(orderitem_price, delta_quantity) {
+$('.order_form select').change(function () {
+   var target = event.target;
+   orderitem_num = parseInt(target.name.replace('orderitems-', '').\
+                                        replace('-product', ''));
+   var orderitem_product_pk = target.options[target.selectedIndex].value;
+
+   if (orderitem_product_pk) {
+       $.ajax({
+           url: "/order/product/" + orderitem_product_pk + "/price/",
+           success: function (data) {
+               if (data.price) {
+                   price_arr[orderitem_num] = parseFloat(data.price);
+                   if (isNaN(quantity_arr[orderitem_num])) {
+                       quantity_arr[orderitem_num] = 0;
+                   }
+                   var price_html = '<span>' + \
+                                    data.price.toString().replace('.', ',') +\
+                                    '</span> руб';
+                   var current_tr = $('.order_form table').\
+                                   find('tr:eq(' + (orderitem_num + 1) + ')');
+
+
+                   current_tr.find('td:eq(2)').html(price_html);
+
+                   if (isNaN(current_tr.find('input[type="number"]').val())) {
+                       current_tr.find('input[type="number"]').val(0);
+                   }
+                   orderSummaryRecalc();
+               }
+           },
+       });
+   }
+});
+
+
+function orderSummaryUpdate(ordered_price, delta_quantity) {
    delta_cost = orderitem_price * delta_quantity;
 
    order_total_cost = Number((order_total_cost + delta_cost).toFixed(2));
