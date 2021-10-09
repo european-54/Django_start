77d5e53f6ff2e0d397ad228dab1f9b12fc61cf90
european <artik-54@mail.ru>
2021-10-09 19:17:32 +0700

    #10.10.2021
    #Lazarev Igor
    #Домашнаяя №5

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -26,6 +26,11 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.name}{self.category.name}"
 
-    @classmethod
-    def get_items(cls):
-        pass
+    #  @classmethod
+    #  def get_items(cls):
+     #   pass
+
+    @staticmethod
+    def get_items():
+        return Product.objects.filter(is_active=True). \
+            order_by('category', 'name')
