71357ffdc83e9142a68370f11a6d1a3c202e19a4
european <artik-54@mail.ru>
2021-10-09 23:50:55 +0700

    #10.10.2021
    #Lazarev Igor
    #Домашнаяя №5

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fvi=54y=^chhsznxs5!j1a56=!zn)dut-^r+t!4ft+rch%wx@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
